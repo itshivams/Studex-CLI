d97497ae9d0d9deec75ce9aedef143be90328cc7
Shivam <143623476+itshivams@users.noreply.github.com>
2026-04-23 14:33:11 +0530

    add: readme

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -1,9 +1,16 @@
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, "README.md")
+long_description = open(readme_path, encoding="utf-8").read() if os.path.exists(readme_path) else ""
 
 setup(
     name="studex-cli",
     version="1.0.1",
     description="A command line interface for Studex platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="itshivams",
     license="MIT",
     url="https://github.com/itshivams/Studex-CLI",
